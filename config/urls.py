60cb24811e43715fd529a6b3bb86cd25a2b2f388
Andy Chang <andyta24@gmail.com>
2023-03-06 13:13:40 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from myapp import views
 urlpatterns = [
 	    #path('admin/', admin.site.urls),
 	    #path('', include('config.urls')),
-            #path('callback/', views.callback),
+            path('callback/', views.callback),
             re_path('^callback', views.callback),
 	]
 
